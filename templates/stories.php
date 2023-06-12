d7032f734973cf4fcbb5fb1e0f7945092238ab32
Ruslan <ru@skylinehealthgroup.com>
2023-06-12 16:45:13 +0300

    add stories template and travel template

---
+++ templates/stories.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * Template Name: stories_page
+ * Template post type: page*/
+?>
+
+<?php
+$args = array(
+    'post_type'      => 'stories',
+    'posts_per_page' => -1,
+    'tax_query'      => array(
+        array(
+            'taxonomy' => 'adventure',
+            'field'    => 'slug',
+            'terms'    => 'first',
+        ),
+    ),
+);
+
+$query = new WP_Query($args);
+?>
+
+<section>
+    <div class="stories_wrapper">
+        <div class="stories">
+            <div class="stories__head">Featured Stories</div>
+            <div class="stories__subhead">Did you read our personal favorites?</div>
+            <div class="stories-content">
+                <?php
+                if ($query->have_posts()) :
+                    while ($query->have_posts()) :
+                        $query->the_post();
+                ?>
+                        <div class="stories-content-block">
+                            <div class="stories-content-block__image">
+                                <?php the_post_thumbnail(); ?>
+                                <div class="stories-content-block__image-autor">
+                                    <?php
+                                    $author_id = get_the_author_meta('ID');
+                                    echo get_avatar($author_id, 30);
+                                    ?>
+                                    <p class="stories-content-block__autor"><?php the_author(); ?></p>
+                                    <p class="stories-content-block__date"><?php the_date(); ?></p>
+                                </div>
+                            </div>
+                            <div class="stories-content-block__text">
+                                <p><?php the_title(); ?></p>
+                            </div>
+                        </div>
+                <?php
+                    endwhile;
+                endif;
+                wp_reset_postdata();
+                ?>
+            </div>
+            
+        </div>
+
+    </div>
+</section>
---
+++ templates/travel.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Template Name: stories_page
+ * Template post type: page*/
+?>
+
+<?php
+$args = array(
+    'post_type' => 'post',
+    'posts_per_page' => 3, // Указать желаемое количество постов для отображения
+    'tag' => 'travel' // Указать желаемый тег для фильтрации постов
+);
+$query = new WP_Query($args);
+?>
+
+<section>
+    <div class="container">
+        <?php
+        if ($query->have_posts()) :
+            while ($query->have_posts()) :
+                $query->the_post();
+        ?>
+                <div class="content">
+                    <div class="content__image">
+                        <div>
+                            <?php the_post_thumbnail(); ?>
+                            <div class="content__head"><?php the_title(); ?></div>
+                            <div class="content-autor">
+                                <?php
+                                $author_id = get_the_author_meta('ID');
+                                echo get_avatar($author_id, 30);
+                                ?>
+                                <div>
+                                    <div class="content-autor__name"><?php the_author(); ?></div>
+                                    <div class="content-autor__date"><?php the_date(); ?></div>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="content__image_text">
+                            <p><?php the_content(); ?></p>
+                        </div>
+                    </div>
+                </div>
+        <?php
+            endwhile;
+        endif;
+        ?>
+
+    </div>
+</section>
+</div>
